d63b3aff9d91607a8e2ed0287b91a3e6707890af
Chris Dostert <chris.dostert@nintex.com>
2017-04-01 15:18:45 -0700

    chore) initial commit

---
+++ suite_test.go
@@ -0,0 +1,13 @@
+package vos
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "virtual-go/vos")
+}
